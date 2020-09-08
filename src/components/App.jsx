d216c11a889c345e022cf8565a7436643ae4a015
Christian Asnel Ngoulla Sob <ngoullasob@gmail.com>
2020-09-08 11:55:40 +0100

    Getting started with components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Heading from './Heading';
+import Paragrpah from './Paragraph';
+
+const App = () => {
+  return (
+    <div>
+      <Heading />
+      <Paragrpah />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Paragraph = () => {
+  return <p>Let's get started with React</p>;
+};
+
+export default Paragraph;
